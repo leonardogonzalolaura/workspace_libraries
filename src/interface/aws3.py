34af60b224e46b8bce82ce2a366d1327eb1db513
Leonardo Daniel Gonzalo Laura <lleonardodaniel@gmail.com>
2024-07-21 18:27:01 +0000

    implement interface aws3

--- src/interface/aws3.py
+++ src/interface/aws3.py
@@ -1,9 +1,20 @@
+import json
 from src.interface.storage import IStorage
+from typing import Union, Dict, Any
 
 class aws_s3(IStorage):
 
     def __init__(self):
         print('aws3')
     
-    def list_file(self):
-        pass
+    def put(self,path:str,name:str,object:Union[str,Dict[str,Any]]):
+        if isinstance(object,str):
+            with open(object,'r') as file:
+                content = file.read()
+        elif isinstance(object,dict):
+            content = json.dumps(object,indent=4)
+        
+        return content
+        
+
+
--- src/interface/storage.py
+++ src/interface/storage.py
@@ -1,17 +1,18 @@
 from abc import ABC, abstractmethod
+from typing import Union, Dict, Any
 
 class IStorage(ABC):
 
     @abstractmethod
-    def get(self):
+    def get(self,path,name):
         pass
 
     @abstractmethod
-    def put(self):
+    def put(self,path:str,name:str,object:Union[str,Dict[str,Any]]):
         pass
 
     @abstractmethod
-    def list(self):
+    def list(self,path:str):
         pass
 
         
